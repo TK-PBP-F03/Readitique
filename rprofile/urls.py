41a2f7e89e037494e0c4d8f197aaef96857eeaef
Kevin I <kevin.ignatius@ui.ac.id>
2023-10-25 22:53:48 +0700

    +added fitur profile\

--- rprofile/urls.py
+++ rprofile/urls.py
@@ -7,5 +7,9 @@ app_name = 'rprofile'
 urlpatterns = [
   path('rprofile/', views.profile, name="profile"),
   path('bookofchoice/', views.bookofchoice, name='bookofchoice'),
+  path('update_email/', views.update_email, name='update_email'),
+  path('update_phone/', views.update_phone, name='update_phone'),
+ 
+  
 
 ]
--- rprofile/views.py
+++ rprofile/views.py
@@ -10,6 +10,9 @@ from django.http import JsonResponse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+
 
 
 @login_required
@@ -20,7 +23,7 @@ def profile(request):
 
     if user.is_superuser:
         role = "Admin"
-    elif user.username in author_names:
+    elif user.username.replace('+',' ') in author_names:
         role = "Writer"
 
     return render(request, 'profile.html', {'user': user, 'role': role})
@@ -37,3 +40,52 @@ def bookofchoice(request):
     
 
     return render(request, 'bookofyourchoice.html', {'book': random_book})
+
+@login_required
+def update_email(request):
+    if request.method == 'POST':
+        user = request.user
+        email = request.POST.get('email')
+        
+        if email:
+            user.email = email
+            user.save()
+            return JsonResponse({'message': 'Email updated successfully'}, status=200)
+        
+        else:
+            return JsonResponse({'error': 'Email data not provided.'}, status=400)
+    else:
+        return JsonResponse({'error': 'Invalid request'}, status=400)
+    
+@login_required
+def update_phone(request):
+    if request.method == 'POST':
+        user = request.user
+        phone = request.POST.get('phone')
+        
+        if phone:
+            user.profile.phone = phone  # Assuming user has a related 'profile' model
+            user.profile.save()
+            return JsonResponse({'message': 'Phone updated successfully'}, status=200)
+        
+        else:
+            return JsonResponse({'error': 'Phone data not provided.'}, status=400)
+    else:
+        return JsonResponse({'error': 'Invalid request'}, status=400)
+    
+def read_book(request, book_id):
+    try:
+        book = Book.objects.get(id=book_id)
+        # Fetch the book content or any relevant data here
+
+        # For simplicity, we'll just send the book's title and description
+        book_data = {
+            'title': book.title,
+            'description': book.description,
+        }
+
+        return JsonResponse(book_data)
+    except Book.DoesNotExist:
+        return JsonResponse({'error': 'Book not found'}, status=404)
+    
+
