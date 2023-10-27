ef8f3524a7b9ca51323a32fb08de84181d9f5070
Kevin I <kevin.ignatius@ui.ac.id>
2023-10-27 15:12:42 +0700

    +class UserProfile\

--- rprofile/models.py
+++ rprofile/models.py
@@ -2,4 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+
 # Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    handphone = models.IntegerField()
+    email = models.TextField()
+  #  profile_picture = models.ImageField(upload_to='profile_pictures/', null=True, blank=True)
+
