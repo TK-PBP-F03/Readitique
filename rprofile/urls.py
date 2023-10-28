dc82c371c74fb6f2f268896f9d2f22851903001b
Kevin I <kevin.ignatius@ui.ac.id>
2023-10-28 14:50:10 +0700

    +not_login\

--- rprofile/urls.py
+++ rprofile/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
   path('json/', views.show_json, name='show_json'), 
   path('json/<int:id>/', views.show_json_by_id, name='show_json_by_id'),
   path('rprofile/update_phone_number/', views.update_phone_number, name='update_phone_number'),
+  path('rprofile/not_login/', views.not_login, name='not_login'),
  
   
 
