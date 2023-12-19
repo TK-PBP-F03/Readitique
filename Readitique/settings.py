b7f47ca3fc0a1668cd0929192cc36b3e9ef0a5db
meteoreinger <reikathr@gmail.com>
2023-12-19 23:45:49 +0700

    an attempt at deployment

--- Readitique/settings.py
+++ Readitique/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -50,7 +51,6 @@ INSTALLED_APPS = [
     'rprofile.apps.RprofileConfig',
     'review.apps.ReviewConfig',
     'authentication',
-    'corsheaders',
     'rest_framework',
 ]
 
@@ -135,9 +135,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+STATIC_ROOT = BASE_DIR / 'static'
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
