7f571f40f454056674a1a95a6b8a720277033e67
rapunzzz <kurniathaariq55@gmail.com>
2023-12-19 20:19:17 +0700

    Add vote

--- addbuku/urls.py
+++ addbuku/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from addbuku.views import add_buku, get_newbook_json, get_filtered, add_newbook_ajax, increment_votes, approve_book, show_json_by_id, show_json, create_newbook_flutter
+from addbuku.views import add_buku, get_newbook_json, get_filtered, add_newbook_ajax, increment_votes, approve_book, show_json_by_id, show_json, create_newbook_flutter, vote_flutter
 
 app_name = 'addbuku'
 urlpatterns = [
@@ -12,4 +12,5 @@ urlpatterns = [
     path('json/', show_json, name='show_json'),
     path('json/<int:id>/', show_json_by_id, name='show_json_by_id'),
     path('create-flutter/', create_newbook_flutter, name='create_newbook_flutter'),
+    path('vote-flutter/', vote_flutter, name='vote_flutter')
 ]
